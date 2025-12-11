a02c08c073801c34181227d6862f8402eb92b603
LucaSain <lucasainenco2@gmail.com>
2025-12-11 23:37:26 +0200

    update

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251211_213706 from './20251211_213706';
+
+export const migrations = [
+  {
+    up: migration_20251211_213706.up,
+    down: migration_20251211_213706.down,
+    name: '20251211_213706'
+  },
+];
