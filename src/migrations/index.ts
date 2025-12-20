84d6486ae0b4d252a695a84176dbcaace572736a
LucaSain <lucasainenco2@gmail.com>
2025-12-20 16:39:27 +0200

    reset migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,9 @@
-
+import * as migration_20251220_143729 from './20251220_143729';
 
 export const migrations = [
-
+  {
+    up: migration_20251220_143729.up,
+    down: migration_20251220_143729.down,
+    name: '20251220_143729'
+  },
 ];
