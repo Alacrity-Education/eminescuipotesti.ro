02862a73ce4d8ee18958e95bf5ac962eda491c49
LucaSain <lucasainenco2@gmail.com>
2025-12-28 21:14:16 +0200

    Add reference links to cards

--- src/blocks/CardBlock/Component.tsx
+++ src/blocks/CardBlock/Component.tsx
@@ -132,7 +132,7 @@ export const CardBlock: React.FC<CardBlockProps & { title?: string }> = ({ title
           );
 
           // Make entire card clickable if link is present
-          return link ? (
+          return link && link.url ? (
             <a href={link.url} key={i} className={cn("block focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary ", spanClasses)}>
               {cardInner}
             </a>
