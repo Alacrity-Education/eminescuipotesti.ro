7b0c34cae429760fb6c573f42e0c90c1f561282b
LucaSain <lucasainenco2@gmail.com>
2025-12-30 00:56:56 +0200

    remove spacing from card block

--- src/blocks/Content/Component.tsx
+++ src/blocks/Content/Component.tsx
@@ -16,7 +16,7 @@ export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
   };
 
   return (
-    <div className="container mx-auto my-4 w-full sm:my-40">
+    <div className="container mx-auto my-4 w-full sm:my-10">
       {title && (
         <h2 className="text-primary py-10 text-center text-xl font-semibold md:text-3xl">
           {title}
